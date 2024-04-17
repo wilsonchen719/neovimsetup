0ca128cfd44941218224a1a4ed1c89fedba0f93b
wilsonchen719 <145998062+wilsonchen719@users.noreply.github.com>
2024-04-18 04:06:47 +0800

    Add more plugins
    1. Get harpoon ready.
    2. Get oil ready.

--- init.lua
+++ init.lua
@@ -3,7 +3,6 @@ vim.g.mapleader = " "
 
 -- Set to true if you have a Nerd Font installed
 vim.g.have_nerd_font = true
-
 -- [[ Setting options ]]
 -- See `:help vim.opt`
 -- NOTE: You can change these options as you wish!
@@ -87,7 +86,7 @@ vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
-vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+vim.keymap.set("n", "<leader>qf", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
@@ -111,7 +110,7 @@ vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left wind
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Explorer View" })
+vim.keymap.set("n", "<leader>pv", "<CMD>Oil<CR>", { desc = "Explorer View" })
 
 -- [[ Basic Autocommands ]
 --  See `:help lua-guide-`
@@ -153,7 +152,7 @@ require("lazy").setup({
 	"jiangmiao/auto-pairs",
 	"BurntSushi/ripgrep",
 	{
-		"smithbm2316/centerpad.nvim",
+		"wilsonchen719/centerpad.nvim",
 		config = function()
 			vim.keymap.set(
 				"n",
@@ -161,6 +160,15 @@ require("lazy").setup({
 				"<cmd>lua require('centerpad').toggle{ leftpad = 30, rightpad = 20 }<cr>",
 				{ silent = true, noremap = true, desc = "Center Editor" }
 			)
+			vim.keymap.set("n", "<leader>q", function()
+				if vim.fn.bufexists("leftpad") then
+					vim.cmd("bw leftpad")
+					vim.cmd("bw rightpad")
+					vim.cmd("wq")
+				else
+					vim.cmd("q")
+				end
+			end)
 			if next(vim.fn.argv()) ~= nil then
 				vim.cmd("Centerpad 30")
 			end
@@ -237,6 +245,7 @@ require("lazy").setup({
 				},
 			})
 			require("notify").setup({
+				background_colour = "#000000",
 				on_open = function(win)
 					vim.api.nvim_win_set_config(win, { focusable = false })
 				end,
@@ -407,7 +416,6 @@ require("lazy").setup({
 			vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
 			vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
 			vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
-			vim.keymap.set("n", "<leader>sg", builtin.git_files, { desc = "[S]earch [G]it File" })
 			--vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
 			vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
 			vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
@@ -415,7 +423,6 @@ require("lazy").setup({
 			vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
 			vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
 			vim.keymap.set("n", "<leader>ss", builtin.lsp_document_symbols, { desc = "[S]earch [S]ymbols" })
-
 			-- Slightly advanced example of overriding default behavior and theme
 			vim.keymap.set("n", "<leader>/", function()
 				-- You can pass additional configuration to Telescope to change the theme, layout, etc.
@@ -433,11 +440,230 @@ require("lazy").setup({
 					prompt_title = "Live Grep in Open Files",
 				})
 			end, { desc = "[S]earch [/] in Open Files" })
-
 			-- Shortcut for searching your Neovim configuration files
 			vim.keymap.set("n", "<leader>sn", function()
 				builtin.find_files({ cwd = vim.fn.stdpath("config") })
 			end, { desc = "[S]earch [N]eovim files" })
+			vim.keymap.set("n", "<leader>sd", function()
+				builtin.find_files({ cwd = vim.fn.stdpath("data") })
+			end, { desc = "[S]earch Neovim [D]" })
+		end,
+	},
+	{
+		"stevearc/oil.nvim",
+		config = function()
+			require("oil").setup({
+				-- Oil will take over directory buffers (e.g. `vim .` or `:e src/`)
+				-- Set to false if you still want to use netrw.
+				default_file_explorer = true,
+				-- Id is automatically added at the beginning, and name at the end
+				-- See :help oil-columns
+				columns = {
+					"icon",
+					-- "permissions",
+					-- "size",
+					-- "mtime",
+				},
+				-- Buffer-local options to use for oil buffers
+				buf_options = {
+					buflisted = false,
+					bufhidden = "hide",
+				},
+				-- Window-local options to use for oil buffers
+				win_options = {
+					wrap = false,
+					signcolumn = "no",
+					cursorcolumn = false,
+					foldcolumn = "0",
+					spell = false,
+					list = false,
+					conceallevel = 3,
+					concealcursor = "nvic",
+				},
+				-- Send deleted files to the trash instead of permanently deleting them (:help oil-trash)
+				delete_to_trash = false,
+				-- Skip the confirmation popup for simple operations (:help oil.skip_confirm_for_simple_edits)
+				skip_confirm_for_simple_edits = false,
+				-- Selecting a new/moved/renamed file or directory will prompt you to save changes first
+				-- (:help prompt_save_on_select_new_entry)
+				prompt_save_on_select_new_entry = true,
+				-- Oil will automatically delete hidden buffers after this delay
+				-- You can set the delay to false to disable cleanup entirely
+				-- Note that the cleanup process only starts when none of the oil buffers are currently displayed
+				cleanup_delay_ms = 2000,
+				lsp_file_methods = {
+					-- Time to wait for LSP file operations to complete before skipping
+					timeout_ms = 1000,
+					-- Set to true to autosave buffers that are updated with LSP willRenameFiles
+					-- Set to "unmodified" to only save unmodified buffers
+					autosave_changes = false,
+				},
+				-- Constrain the cursor to the editable parts of the oil buffer
+				-- Set to `false` to disable, or "name" to keep it on the file names
+				constrain_cursor = "editable",
+				-- Set to true to watch the filesystem for changes and reload oil
+				experimental_watch_for_changes = false,
+				-- Keymaps in oil buffer. Can be any value that `vim.keymap.set` accepts OR a table of keymap
+				-- options with a `callback` (e.g. { callback = function() ... end, desc = "", mode = "n" })
+				-- Additionally, if it is a string that matches "actions.<name>",
+				-- it will use the mapping at require("oil.actions").<name>
+				-- Set to `false` to remove a keymap
+				-- See :help oil-actions for a list of all available actions
+				keymaps = {
+					["g?"] = "actions.show_help",
+					["<CR>"] = "actions.select",
+					["<C-s>"] = "actions.select_vsplit",
+					["<C-h>"] = "actions.select_split",
+					["<C-t>"] = "actions.select_tab",
+					["<C-p>"] = "actions.preview",
+					["<C-c>"] = "actions.close",
+					["<C-l>"] = "actions.refresh",
+					["-"] = "actions.parent",
+					["_"] = "actions.open_cwd",
+					["`"] = "actions.cd",
+					["~"] = "actions.tcd",
+					["gs"] = "actions.change_sort",
+					["gx"] = "actions.open_external",
+					["g."] = "actions.toggle_hidden",
+					["g\\"] = "actions.toggle_trash",
+				},
+				-- Configuration for the floating keymaps help window
+				keymaps_help = {
+					border = "rounded",
+				},
+				-- Set to false to disable all of the above keymaps
+				use_default_keymaps = true,
+				view_options = {
+					-- Show files and directories that start with "."
+					show_hidden = false,
+					-- This function defines what is considered a "hidden" file
+					is_hidden_file = function(name, bufnr)
+						return vim.startswith(name, ".")
+					end,
+					-- This function defines what will never be shown, even when `show_hidden` is set
+					is_always_hidden = function(name, bufnr)
+						return false
+					end,
+					-- Sort file names in a more intuitive order for humans. Is less performant,
+					-- so you may want to set to false if you work with large directories.
+					natural_order = true,
+					sort = {
+						-- sort order can be "asc" or "desc"
+						-- see :help oil-columns to see which columns are sortable
+						{ "type", "asc" },
+						{ "name", "asc" },
+					},
+				},
+				-- Configuration for the floating window in oil.open_float
+				float = {
+					-- Padding around the floating window
+					padding = 2,
+					max_width = 0,
+					max_height = 0,
+					border = "rounded",
+					win_options = {
+						winblend = 0,
+					},
+					-- This is the config that will be passed to nvim_open_win.
+					-- Change values here to customize the layout
+					override = function(conf)
+						return conf
+					end,
+				},
+				-- Configuration for the actions floating preview window
+				preview = {
+					-- Width dimensions can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+					-- min_width and max_width can be a single value or a list of mixed integer/float types.
+					-- max_width = {100, 0.8} means "the lesser of 100 columns or 80% of total"
+					max_width = 0.9,
+					-- min_width = {40, 0.4} means "the greater of 40 columns or 40% of total"
+					min_width = { 40, 0.4 },
+					-- optionally define an integer/float for the exact width of the preview window
+					width = nil,
+					-- Height dimensions can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+					-- min_height and max_height can be a single value or a list of mixed integer/float types.
+					-- max_height = {80, 0.9} means "the lesser of 80 columns or 90% of total"
+					max_height = 0.9,
+					-- min_height = {5, 0.1} means "the greater of 5 columns or 10% of total"
+					min_height = { 5, 0.1 },
+					-- optionally define an integer/float for the exact height of the preview window
+					height = nil,
+					border = "rounded",
+					win_options = {
+						winblend = 0,
+					},
+					-- Whether the preview window is automatically updated when the cursor is moved
+					update_on_cursor_moved = true,
+				},
+				-- Configuration for the floating progress window
+				progress = {
+					max_width = 0.9,
+					min_width = { 40, 0.4 },
+					width = nil,
+					max_height = { 10, 0.9 },
+					min_height = { 5, 0.1 },
+					height = nil,
+					border = "rounded",
+					minimized_border = "none",
+					win_options = {
+						winblend = 0,
+					},
+				},
+				-- Configuration for the floating SSH window
+				ssh = {
+					border = "rounded",
+				},
+			})
+		end,
+	},
+	{
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local harpoon = require("harpoon")
+			harpoon.setup({})
+			local function toggle_telescope(harpoon_files)
+				local file_paths = {}
+				for _, item in ipairs(harpoon_files.items) do
+					table.insert(file_paths, item.value)
+				end
+
+				require("telescope.pickers")
+					.new({}, {
+						prompt_title = "Harpoon",
+						finder = require("telescope.finders").new_table({
+							results = file_paths,
+						}),
+						previewer = conf.file_previewer({}),
+						sorter = conf.generic_sorter({}),
+					})
+					:find()
+			end
+
+			vim.keymap.set("n", "<C-e>", function()
+				harpoon.ui:toggle_quick_menu(harpoon:list())
+			end, { desc = "Open harpoon window" })
+			vim.keymap.set("n", "<leader>a", function()
+				harpoon:list():add()
+			end)
+			-- vim.keymap.set("n", "<C-h>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+			vim.keymap.set("n", "<C-i>", function()
+				harpoon:list():select(1)
+			end)
+			vim.keymap.set("n", "<C-o>", function()
+				harpoon:list():select(2)
+			end)
+			vim.keymap.set("n", "<C-p>", function()
+				harpoon:list():select(3)
+			end)
+			-- Toggle previous & next buffers stored within Harpoon list
+			vim.keymap.set("n", "<C-z>", function()
+				harpoon:list():prev()
+			end)
+			vim.keymap.set("n", "<C-x>", function()
+				harpoon:list():next()
+			end)
 		end,
 	},
 	{ -- LSP Configuration & Plugins
