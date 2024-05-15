d6092a4ad9be72c97c4804399010847512694786
wilsonchen719 <145998062+wilsonchen719@users.noreply.github.com>
2024-05-15 19:17:12 +0800

    Fix bug: Bookmark shortcuts fixed. Add feature: Dashboard ramdomly shows one pic.

--- lua/wilsonchen/bookmark.lua
+++ lua/wilsonchen/bookmark.lua
@@ -5,6 +5,7 @@ return {
 	config = function()
 		local bookmarks = require("bookmarks")
 		bookmarks.setup({
+			mappings_enabled = false,
 			keymap = {},
 		})
 		require("telescope").load_extension("bookmarks")
@@ -15,10 +16,13 @@ return {
 			bookmarks.delete_on_virt()
 		end, { desc = "[B]ookmark [D]elete" })
 		vim.keymap.set("n", "<leader>bs", function()
-			bookmarks.delete_on_virt()
+			bookmarks.show_desc()
 		end, { desc = "[B]ookmark [S]how" })
 		vim.keymap.set("n", "<leader>sm", function()
 			vim.cmd("Telescope bookmarks")
 		end, { desc = "[S]earch [B]ookmarks" })
+		vim.keymap.set("n", "<tab><tab>", function()
+			bookmarks.toggle_bookmarks()
+		end, { desc = "[T]oggle Bookmarks" })
 	end,
 }
