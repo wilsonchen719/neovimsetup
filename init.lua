3e074644b3569a2569accea56d743835640e3925
wilsonchen719 <145998062+wilsonchen719@users.noreply.github.com>
2024-06-18 14:32:55 +0800

    Few changes

--- init.lua
+++ init.lua
@@ -1110,16 +1110,16 @@ require("lazy").setup({
 		config = function()
 			local opts = {
 				notify_on_error = false,
-				format_on_save = function(bufnr)
-					-- Disable "format_on_save lsp_fallback" for languages that don't
-					-- have a well standardized coding style. You can add additional
-					-- languages here or re-enable it for the disabled ones.
-					local disable_filetypes = { c = true, cpp = true }
-					return {
-						timeout_ms = 1000,
-						lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-					}
-				end,
+				-- format_on_save = function(bufnr)
+				-- 	-- Disable "format_on_save lsp_fallback" for languages that don't
+				-- 	-- have a well standardized coding style. You can add additional
+				-- 	-- languages here or re-enable it for the disabled ones.
+				-- 	local disable_filetypes = { c = true, cpp = true }
+				-- 	return {
+				-- 		timeout_ms = 1000,
+				-- 		lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+				-- 	}
+				-- end,
 				formatters_by_ft = {
 					lua = { "stylua" },
 					python = { "black" },
